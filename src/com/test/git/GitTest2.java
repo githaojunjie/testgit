853aef2b82d4c895bc70ecd2ac5ec681b33d6ed8
githaojunjie <1397326345@qq.com>
2019-12-19 21:10:05 +0800

    测试JavaGit

--- src/com/test/git/GitTest2.java
+++ src/com/test/git/GitTest2.java
@@ -1,8 +1,7 @@
 package com.test.git;
 
-public class GitTest {
+public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("helloGit");
         System.out.println("helloGit2");
     }
 }
