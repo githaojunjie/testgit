1bd31d58be50cde29104d4cd37a094be095ea59e
gitjunjiehao <jun_jiehao@163.com>
2019-12-19 21:47:24 +0800

    000000

--- src/com/test/git/GitTest2.java
+++ src/com/test/git/GitTest2.java
@@ -3,6 +3,6 @@ package com.test.git;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("helloGit2");
-        System.out.println("helloGit2gitjunjiehao");
+        System.out.println("0000000");
     }
 }
